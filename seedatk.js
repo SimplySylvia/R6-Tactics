ee691820c512a7774a8c4c7ecad34238640c1e6d
DaltonHart <daltonvaren@gmail.com>
2018-07-07 15:26:28 -0700

    created seed file for all  defops

--- seedatk.js
+++ seedatk.js
@@ -90,7 +90,7 @@ var atkopsList = [{
         img : '/images/ALDA556.png',
       },
       pri2 : {
-        name : 'Alda 5.56',
+        name : 'ACS 12',
         img : '/images/acs12.png',
       },
       pri3 : {
@@ -123,101 +123,14 @@ var atkopsList = [{
     Organization : 'G.I.S',
   }]
 
-// db.Atkop.remove({}, function(err, atkops){
-//     // code in here runs after all posts are removed
-//     db.Atkop.create(atkopsList, function(err, atkops){
-//       // code in here runs after all posts are created
-//       if (err) { return console.log('ERROR', err); }
-//       console.log("all atkops:", atkops);
-//       console.log("created", atkops.length, "atkops");
-//     });
-    
-//   });
-
-  db.Defop.remove({}, function(err, defops){
+db.Atkop.remove({}, function(err, atkops){
     // code in here runs after all posts are removed
-    db.Defop.create(defopsList, function(err, defops){
+    db.Atkop.create(atkopsList, function(err, atkops){
       // code in here runs after all posts are created
       if (err) { return console.log('ERROR', err); }
-      console.log("all defops:", defops);
-      console.log("created", defops.length, "defops");
-      process.exit();evil
+      console.log("all atkops:", atkops);
+      console.log("created", atkops.length, "atkops");
+      process.exit();
     });
     
   });
-
-  var SmapList = [
-    {
-    name : 'House',
-    coverimg: 'placeholder',
-    floors : [
-      {
-        name: 'Basement',
-        img: 'placeholder',
-      },
-      {
-        name: '1st',
-        img: 'placeholder',
-      },
-      {
-        name: '2nd',
-        img: 'placeholder',
-      },
-      {
-        name: 'Roof',
-        img: 'placeholder',
-      },
-      {
-        name: 'NA',
-        img: 'placeholder',
-      },
-    ],
-    atkspawns : {
-      atkspawn1 : 'APC Area',
-      atkspawn2 : 'Construction Site',
-      atkspawn3 : 'Side Street',
-      atkspawn4 : 'River Docks',
-      },
-    securearea : {
-      area1 : 'Garage',
-      area2 : 'Living Room',
-      area3 : 'Kids Bedroom',
-      area4 : 'Master Bedroom',
-      },
-    bombsites : [
-      {
-      assignment : 1,
-      a : 'Workshop',
-      b : 'Kids Bedroom',
-      },
-      {
-      assignment : 2,
-      a : 'Training Room',
-      b : 'Living Room',
-      },
-      {
-      assignment : 3,
-      a : 'Training Room',
-      b : 'Garage',
-      },
-    ],
-    hostage : {
-      area1 : 'Laundry Room',
-      area2 : 'Dining Room',
-      area3 : 'Workshop',
-      area4 : 'Master Bedroom',
-    },
-  }
-];
-
-// db.Smap.remove({}, function(err, smaps){
-//   // code in here runs after all posts are removed
-//   db.Smap.create(SmapList, function(err, smaps){
-//     // code in here runs after all posts are created
-//     if (err) { return console.log('ERROR', err); }
-//     console.log("all Smaps:", smaps);
-//     console.log("created", smaps.length, "smaps");
-//     process.exit();
-//   });
-  
-// });
