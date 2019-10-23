a66f5cf547b7a9436a658f29874e4dc30c90fee6
daltonhart <daltonvaren@gmail.com>
2019-10-23 14:45:29 -0700

    refactors middleware to handle count order

--- middleware/response.js
+++ middleware/response.js
@@ -3,16 +3,21 @@ module.exports = (req, res, next) => {
     const resObj = {
       status,
       requestedAt: new Date().toLocaleString(),
+      count: null,
       data
     };
     if (data === null) {
       delete resObj.data;
+      delete resObj.count;
       resObj.message = 'No Documents Found';
+    } else if (typeof data === 'object') {
+      delete resObj.count;
     } else if (data.constructor === Array) {
       if (data.length > 0) {
         resObj.count = data.length;
       } else {
         delete resObj.data;
+        delete resObj.count;
         resObj.message = 'No Documents Found';
       }
     }
