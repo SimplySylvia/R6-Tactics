19d60f6c6fe584d38eefa170048b74e2db8857c2
daltonhart <daltonvaren@gmail.com>
2019-10-23 13:50:37 -0700

    adds filter type operator routes

--- routes/api/operators.js
+++ routes/api/operators.js
@@ -4,5 +4,7 @@ const controllers = require('../../controllers');
 
 module.exports = router
   .get('/', controllers.operator.index)
+  .get('/attack', controllers.operator.filterAttack)
+  .get('/defense', controllers.operator.filterDefense)
   .post('/', controllers.operator.create)
   .get('/:id', controllers.operator.show);
