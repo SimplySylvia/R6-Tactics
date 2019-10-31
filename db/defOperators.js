a6fa0f3630d9bc9865e27b95fee49bc046e056d1
daltonhart <daltonvaren@gmail.com>
2019-10-31 11:17:20 -0700

    adds fix for defense model name

--- db/defOperators.js
+++ db/defOperators.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     name: 'Maestro',
     badge: '/files/operators/def/badges/maestrobadge.png',
-    img: '/files/operators/def/profile/maestro.png',
+    image: '/files/operators/def/profile/maestro.png',
     bio:
       'Maestro is oldest in a family of eight children. He grew up in Rome, but attended the Military Academy of Modena at 18. He then joined the Carabinieri and earned a spot as an Explorer Paratrooper in the 1st Paratrooper Regiment known as Tuscania.',
     ability: {
@@ -47,7 +47,7 @@ module.exports = [
   {
     name: 'Alibi',
     badge: '/files/operators/def/badges/alibibadge.png',
-    img: '/files/operators/def/profile/alibi.png',
+    image: '/files/operators/def/profile/alibi.png',
     bio:
       'Alibi was born in Tripoli, Libya and immigrated with her family when she was three years old. Her father managed a small ordnance manufacturer, using his contacts to open up exports.',
     ability: {
@@ -92,7 +92,7 @@ module.exports = [
   {
     name: 'Vigil',
     badge: '/files/operators/def/badges/vigilbadge.png',
-    img: '/files/operators/def/profile/vigil.png',
+    image: '/files/operators/def/profile/vigil.png',
     bio:
       'Chul Kyung Hwa spent months on the run in Asia as a child. He lost his family during the flight before finding asylum in South Korea. As an adult, he enlisted in the ROK Navy and later earned a berth in the ROKN UDT/SEALs, becoming so highly skilled that the 707th Special Mission Battalion handpicked him as an operator. His ERC-7 backpack enables him to vanish from live feeds.',
     ability: {
@@ -137,7 +137,7 @@ module.exports = [
   {
     name: 'Ela',
     badge: '/files/operators/def/badges/elabadge.png',
-    img: '/files/operators/def/profile/ela.png',
+    image: '/files/operators/def/profile/ela.png',
     bio:
       'Elzbieta Bosak is the newest addition to Team Rainbow. Nicknamed Ela, this driven 30-year-old has had an unconventional progression. Having quickly risen among Poland’s Special Forces, she was deployed in six countries and active in Iraq for seven years.Her exploits should come as no surprise, considering that she is the youngest daughter of Jan Bosak, a former commander of Poland’s counter-terrorism unit GROM. Despite this legacy, Ela is not the stiff soldier one would expect. Ela experienced some difficulties while attending a military high school. She was bullied constantly due to the special attention she received as the GROM Commander’s daughter.',
     ability: {
@@ -178,7 +178,7 @@ module.exports = [
   {
     name: 'Lesion',
     badge: '/files/operators/def/badges/lesionbadge.png',
-    img: '/files/operators/def/profile/lesion.png',
+    image: '/files/operators/def/profile/lesion.png',
     bio:
       'Born in Junk Bay, Liu Tze Long, codenamed Lesion, was raised by his father, who worked for a ship breaking company. During these years, Lesion would stay at the shipyard, and help him dismantle obsolete ships for scrapping. Crumbling with debts after his father was injured, Lesion took on various hazardous jobs in demolition until he found an opportunity for mine clearance duty. Demonstrating a natural calm while handling highly corrosive substances, he joined the Explosive Ordnance Disposal Bureau. He is a known expert in chemical, biological, radiological and nuclear (CBRN) terrorist threats.',
     ability: {
@@ -219,7 +219,7 @@ module.exports = [
   {
     name: 'Mira',
     badge: '/files/operators/def/badges/mirabadge.png',
-    img: '/files/operators/def/profile/mira.png',
+    image: '/files/operators/def/profile/mira.png',
     bio:
       'Elena María Álvarez was born in Retiro, Madrid, where she learned to spot and fix mechanical defects on automobile engines. Demonstrating solid physical endurance and a stubborn need to prove her worth, Álvarez later joined the Grupo Especial de Operaciones (G.E.O.). Her wide-ranging knowledge of vehicles made her an expert in the ballistic field, which she uses to develop and test bulletproof materials.',
     ability: {
@@ -264,7 +264,7 @@ module.exports = [
   {
     name: 'Echo',
     badge: '/files/operators/def/badges/echobadge.png',
-    img: '/files/operators/def/profile/echo.png',
+    image: '/files/operators/def/profile/echo.png',
     bio:
       'Known as a natural prodigy in robotics, Masaru Enatsu grew up in Suginami City, Tokyo, the birthplace of several animation studios and technology companies. Enticed to test his devices on the field, he joined the National Police Academy. Specialised in Recon operations, he was recruited by the Tokyo Metropolitan Police Department Special Unit before transferring to the Aichi Unit.',
     ability: {
@@ -309,7 +309,7 @@ module.exports = [
   {
     name: 'Caveira',
     badge: '/files/operators/def/badges/caveirabadge.png',
-    img: '/files/operators/def/profile/caveira.png',
+    image: '/files/operators/def/profile/caveira.png',
     bio:
       'Known as a brutal interrogator, Taina “Caveira” Pereira’s makes for a fearless Operator on the field. Originally from a small town on the outskirts of São Paulo, her life could have taken a turn for the worst when she was arrested at 16 on robbery charges. Her promising profile allowed for an exception, giving her the option between entering a juvenile reformatory and working with BOPE.',
     ability: {
@@ -350,7 +350,7 @@ module.exports = [
   {
     name: 'Valkyrie',
     badge: '/files/operators/def/badges/valkyriebadge.png',
-    img: '/files/operators/def/profile/valkyrie.png',
+    image: '/files/operators/def/profile/valkyrie.png',
     bio:
       'While serving as a Naval Intelligence Officer, Castellano was chosen to be one of the first women admitted to SEALs training. Singularly focused and driven, she will do whatever it takes to accomplish the mission in the most seamless and effective way possible. She uses the “Black Eye”, a camera able to adhere to nearly any surface, while maintaining a clear view and providing her whole team with a live video feed. The quick deploy camera has proved incredibly useful for operations in situations with limited intel, gaining a much needed tactical advantage in close quarters or around blind spots.',
     ability: {
@@ -391,7 +391,7 @@ module.exports = [
   {
     name: 'Frost',
     badge: '/files/operators/def/badges/frostbadge.png',
-    img: '/files/operators/def/profile/frost.png',
+    image: '/files/operators/def/profile/frost.png',
     bio:
       'A combat systems officer, Tina Lin Tsang is a focused and meticulous planner. She uses the Sterling Mk2 LHT or “Welcome Mat”, a floor mounted, rubber and metallic spring loaded leg-hold trap that provides her with a non-lethal means of immobilising her prey. The sleek design of the “Welcome Mat” allows for seamless deployment in a variety of areas and environments without the need for extensive camouflage.',
     ability: {
@@ -432,7 +432,7 @@ module.exports = [
   {
     name: 'Mute',
     badge: '/files/operators/def/badges/mutebadge.png',
-    img: '/files/operators/def/profile/mute.png',
+    image: '/files/operators/def/profile/mute.png',
     bio:
       "A veteran of Britain's Government Communications Headquarters (GCHQ) unit, Mark Chandar’s exceptional work as a signals intelligence (sigint) specialist has made him a valuable addition to Rainbow.",
     ability: {
@@ -473,7 +473,7 @@ module.exports = [
   {
     name: 'Smoke',
     badge: '/files/operators/def/badges/smokebadge.png',
-    img: '/files/operators/def/profile/smoke.png',
+    image: '/files/operators/def/profile/smoke.png',
     bio:
       'Porter is a thrill seeker with very little regard for his personal safety. He is fearless in battle and can be a great asset for defensive maneuvers, however he seems to enjoy the havoc and chaos of war.',
     ability: {
@@ -518,7 +518,7 @@ module.exports = [
   {
     name: 'Castle',
     badge: '/files/operators/def/badges/castlebadge.png',
-    img: '/files/operators/def/profile/castle.png',
+    image: '/files/operators/def/profile/castle.png',
     bio:
       'Miles Campbell holds a BA in Criminal Justice and is a former LAPD SWAT officer who went federal and is now an FBI SWAT veteran.',
     ability: {
@@ -563,7 +563,7 @@ module.exports = [
   {
     name: 'Pulse',
     badge: '/files/operators/def/badges/pulsebadge.png',
-    img: '/files/operators/def/profile/pulse.png',
+    image: '/files/operators/def/profile/pulse.png',
     bio:
       "Jack Estrada's work with the FBI biometrics program allowed him to blend his background in communications and biochemistry to a powerful effect.",
     ability: {
@@ -608,7 +608,7 @@ module.exports = [
   {
     name: 'Doc',
     badge: '/files/operators/def/badges/docbadge.png',
-    img: '/files/operators/def/profile/doc.png',
+    image: '/files/operators/def/profile/doc.png',
     bio:
       'Kateb grew up in an affluent family, in Paris’ 16th arrondissement. At twenty, he abandoned a potentially prominent private medical practice in favor of a career with the French defense Health Service. Gustave Kateb is cut from a rare cloth: a genuine altruist, a true believer in making the world a safer place for his fellow man.',
     ability: {
@@ -657,7 +657,7 @@ module.exports = [
   {
     name: 'Rook',
     badge: '/files/operators/def/badges/rookbadge.png',
-    img: '/files/operators/def/profile/rook.png',
+    image: '/files/operators/def/profile/rook.png',
     bio:
       "Julien Nizan is the deliverer of the GIGN's famous trust exercise for new recruits: taking a bullet fired by one of your colleagues with only a small armor plate to protect you. He carries these same armor plates into combat, dropping them for his allies to reinforce their defenses. ",
     ability: {
@@ -706,7 +706,7 @@ module.exports = [
   {
     name: 'Kapkan',
     badge: '/files/operators/def/badges/kapkanbadge.png',
-    img: '/files/operators/def/profile/kapkan.png',
+    image: '/files/operators/def/profile/kapkan.png',
     bio:
       'Born and raised in Kovrov, Basuda is the son of factory workers. To escape a life in the factory, he enlisted with the Ministry of Internal Affairs and was sent to a small port town on the Barents Sea. Basuda became an avid hunter and trapper during his time in the Arctic Circle.',
     ability: {
@@ -751,7 +751,7 @@ module.exports = [
   {
     name: 'Tachanka',
     badge: '/files/operators/def/badges/tachankabadge.png',
-    img: '/files/operators/def/profile/tachanka.png',
+    image: '/files/operators/def/profile/tachanka.png',
     bio:
       'Alexsandr Senaviev is an iconoclast who came of age in a pre-glasnost red army family and has retained an interest in soviet machinery and weaponry.',
     ability: {
@@ -796,7 +796,7 @@ module.exports = [
   {
     name: 'Jager',
     badge: '/files/operators/def/badges/jagerbadge.png',
-    img: '/files/operators/def/profile/jager.png',
+    image: '/files/operators/def/profile/jager.png',
     bio:
       'A veteran of GSG9 anti-piracy ops in the Indian Ocean, Marius Streicher has seen action as a helicopter operator and technology advisor.',
     ability: {
@@ -837,7 +837,7 @@ module.exports = [
   {
     name: 'Bandit',
     badge: '/files/operators/def/badges/banditbadge.png',
-    img: '/files/operators/def/profile/bandit.png',
+    image: '/files/operators/def/profile/bandit.png',
     bio:
       'A former undercover agent within the Hannover Hells Angels, Brunsmeier is known for taking obsolete items and repurposing them into weapons. He has cobbled together a device that can electrify barbed wires and barricades, and injure or destroy anything that comes into contact with it.',
     ability: {
@@ -878,7 +878,7 @@ module.exports = [
   {
     name: 'Clash',
     badge: '/files/operators/def/badges/clashbadge.png',
-    img: '/files/operators/def/profile/clash.png',
+    image: '/files/operators/def/profile/clash.png',
     bio:
       'Born and raised in England, Clash is a tough officer who served with the Territorial Support Group, holding the front line during the 2011 London Riots. She earned her spot with the Specialist Firearms Command (SCO19), making herself known within London’s Metropolitan Police Services (MPS).',
     ability: {
