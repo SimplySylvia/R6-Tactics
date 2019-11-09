8968ee6ad18ac760afac8805c03817275e9e8c21
daltonhart <daltonvaren@gmail.com>
2019-11-08 16:42:44 -0800

    adds log

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,9 +1,28 @@
+const buildLogs = (logs, current_key) => {
+  return logs
+    .map(
+      log =>
+        `
+        <tr>
+          <td class='ui center'>${log.requestMethod}</td>
+          <td>${log.requestUrl}</td>
+          <td>${new Date(log.date).toLocaleString()}</td>
+          <td class="${log.api_key === current_key ? '' : 'archive'}">${
+          log.api_key
+        }</td>
+        </tr>`
+    )
+    .join('');
+};
+
 const setProfile = res => {
+  console.log(res);
   $('#api_key').append(`<code>${res.data.api_key}</code>`);
   res.data.archive_keys.forEach(key => {
     const template = `<section class="doccode archive"><code>${key}</code></section>`;
     $('#archived-keys').append(template);
   });
+  $('#logs').append(buildLogs(res.data.logs, res.data.api_key));
 };
 
 $.ajax({
